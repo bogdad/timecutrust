f9e82682885b3cd753dfd78f3b4ada61ddfb810a
Vladimir Shakhov <vshakhov@yandex-team.ru>
2015-07-06 19:02:49 +0300

    added a function

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,18 @@ fn main() {
     readlines();
 }
 
+fn matches(re: &Regex, line: &str) -> bool {
+    re.is_match(line)
+}
+
+
+
 fn readlines() {
     let stdin = io::stdin();
     let re = Regex::new(r"^\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\]").unwrap();
     for r_line in stdin.lock().lines() {
         let line = r_line.unwrap(); 
-        if re.is_match(&line) {
+        if matches(&re, &line) {
             print!("{}\n", line);
         }
     }
