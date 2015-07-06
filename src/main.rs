1cd29a79f2be9a7b005a6782d4bceb646cb9d6ec
Vladimir Shakhov <vshakhov@yandex-team.ru>
2015-07-06 22:48:39 +0300

    rewrote, failing

--- src/main.rs
+++ src/main.rs
@@ -16,44 +16,44 @@ fn matches(re: &Regex, line: &str) -> bool {
     re.is_match(line)
 }
 
-fn get_first_line_after(f: &File, from: SeekFrom) {
+fn get_first_line_after(f: &File, from: u64) -> String {
     let cur = from;
-    
+    String::new()
 }
 
-const SIZE: u32 = 512;
-fn find_new_line_pos<'a, R: Read + Seek>(reader: &mut BufReader<R>, from: u64) -> Option<usize> { 
-    let before = match from.checked_sub(SIZE as u64) {
+const SIZE: usize = 512;
+fn find_new_line_pos<'a, R: Read + Seek>(reader: &mut BufReader<R>, from: usize) -> Option<String> { 
+    let before = match from.checked_sub(SIZE) {
         None => 0,
         Some(x) => x
     };
-    reader.seek(SeekFrom::Start(before)).unwrap();
-    let mut buf: [u8;SIZE as usize] = [0; SIZE as usize];
+    reader.seek(SeekFrom::Start(before as u64)).unwrap();
+    let mut buf: [u8;SIZE] = [0; SIZE];
     let len = reader.read(&mut buf).unwrap();
-    let last_line = buf[0..len].iter().rposition(|x|*x==0);
+    let last_line = buf[0..len].iter().rposition(|x|*x==b'\n');
     match last_line {
         None => { 
-                    let new_before = before.checked_sub(SIZE as u64);
+                    let new_before = before.checked_sub(SIZE);
                     match new_before {
                         Some(x) => find_new_line_pos(reader, x),
                         None => None
                     }
                 },
-        Some(pos) => Some(pos + 1)
+        Some(pos) => Some(String::from_utf8_lossy(&buf[pos..from]))
     }
 }
 
 #[test]
 fn find_new_line_pos_works() {
-    let data = vec![1,2,0,1,0,5];
+    let mut data =String::from_str("some\nother\nline").as_bytes();
     let mut test_data = BufReader::new(Cursor::new(data));
     {
         let pos = find_new_line_pos(&mut test_data, 5);
-        assert_eq!(Some(5),pos);
+        assert_eq!(Some("line".as_bytes()),pos);
     }
     {
         let pos = find_new_line_pos(&mut test_data, 4);
-        assert_eq!(Some(5), pos);
+        assert_eq!(Some("linr".as_bytes()), pos);
     }
 }
 
